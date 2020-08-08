4707e811c21f5707f3c9e821eff4efa63ef95756
Acidiney Dias <acidiney.dias@ideiasdinamicas.com>
2020-08-08 22:10:44 +0100

    fix: style guide

--- src/app/views/users/user-list/user-list.component.ts
+++ src/app/views/users/user-list/user-list.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 import { ApplicationService } from '../../../api/application.service';
 
 @Component({
-  selector: "app-user-list",
-  templateUrl: "./user-list.component.html",
-  styleUrls: ["./user-list.component.css"],
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css'],
 })
 export class UserListComponent implements OnInit {
   users: any = [];
--- src/app/views/users/users.module.ts
+++ src/app/views/users/users.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core'
-import { FormsModule } from '@angular/forms'
-import { CommonModule } from '@angular/common'
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
-import { UsersRoutingModule } from './users-routing.module'
-import { UsersComponent } from './users.component'
-import { UserFormComponent } from './user-form/user-form.component'
-import { UserListComponent } from './user-list/user-list.component'
+import { UsersRoutingModule } from './users-routing.module';
+import { UsersComponent } from './users.component';
+import { UserFormComponent } from './user-form/user-form.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   imports: [
